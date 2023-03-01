7b932ee243b1f50a16ef863de96b5f15d3709383
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-03-01 14:08:51 -0300

    update rotas main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { KanbanComponent } from './modules/kanban/kanban.component';
 import { TodoListComponent } from './modules/todo-list/todo-list.component';
 
 const routes: Routes = [
   {
     path:'',
     pathMatch: 'full',
+    redirectTo: 'todo'
   },
   {
     path:'todo',
     loadChildren:() => import('./modules/todo-list/todo-list.module').then(m => m.TodoListModule) 
   },
-  {
-    path:'kanban',
-    loadChildren: () => import('./modules/kanban/kanban.module').then(m => m.KanbanModule)
-  }
 ];
 
 @NgModule({
